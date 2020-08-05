e663473cf022d8cf91eb2781e9ef7079aefede45
Nishant Ranjan <nishantranjan1998@gmail.com>
2020-08-06 00:57:58 +0530

    UI changes for homepage

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import lemonImg from './lemon.png';
+
 import './Join.css';
 
 const Join = () => {
@@ -10,7 +12,9 @@ const Join = () => {
     return (
         <div className="joinOuterContainer">
             <div className="joinInnerContainer">
-                <h1 className="heading">Join</h1>
+                <img src={lemonImg} className="lemon" alt="lemon logo" />
+                <h1 className="heading">Lemon Chat</h1>
+                <br />
                 <div>
                     <input placeholder="Name" className="joinInput" type="text"
                         onChange={(event) => setName(event.target.value)} />
@@ -21,7 +25,7 @@ const Join = () => {
                 </div>
                 <Link onClick={event => (!name || !room) ? event.preventDefault() : null}
                     to={`/chat?name=${name}&room=${room}`}>
-                    <button className={'button mt-20'} type="submit">Sign In</button>
+                    <button className={'button mt-20'} type="submit">Join</button>
                 </Link>
             </div>
         </div>
